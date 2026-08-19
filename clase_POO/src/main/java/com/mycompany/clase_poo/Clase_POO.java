b1645767e8da92c09b8c3bd93be0049719fca980
Jhonayker777 <jhon117.hello@gmail.com>
2026-08-19 14:04:00 -0500

    creation proyect

---
+++ clase_POO/src/main/java/com/mycompany/clase_poo/Clase_POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase_poo;
+
+/**
+ *
+ * @author jhon1
+ */
+public class Clase_POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
