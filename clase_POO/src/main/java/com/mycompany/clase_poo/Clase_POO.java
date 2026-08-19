f0f31a415ffd7a97c7f0dd7fd455b811c19fed36
Jhonayker777 <jhon117.hello@gmail.com>
2026-08-19 14:25:04 -0500

    New class Estudiante

--- clase_POO/src/main/java/com/mycompany/clase_poo/Clase_POO.java
+++ clase_POO/src/main/java/com/mycompany/clase_poo/Clase_POO.java
@@ -11,6 +11,9 @@ package com.mycompany.clase_poo;
 public class Clase_POO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Estudiante estudiante1 = new Estudiante();
+        estudiante1.nombre="Jhonayker";
+        estudiante1.edad=18;
+        estudiante1.mostrar();
     }
 }
---
+++ clase_POO/src/main/java/com/mycompany/clase_poo/Estudiante.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clase_poo;
+
+/**
+ *
+ * @author jhon1
+ */
+public class Estudiante {
+    
+    String nombre;
+    int edad;
+    
+    public void mostrar(){
+    System.out.println("nombre: "+ nombre);
+    System.out.println("edad: "+ edad);
+    
+    }
+}
+
+
+
